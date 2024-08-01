8cdcdf2346d71a5ac543080c3c31341595588566
Dianxin Luan <49792786+dianixn@users.noreply.github.com>
2024-08-01 18:08:52 +0100

    Delete Naren_Channel/getLocalPhiBS.m

--- Naren_Channel/getLocalPhiBS.m
+++
@@ -1,9 +0,0 @@
-function local_phi = getLocalPhiBS(theta,phi,alpha,beta,gamma) 
-% (M x N) x 1
-
-local_phi = angle(cosd(beta).*sind(theta).*cosd(phi-alpha)-sind(beta).*cosd(theta)+...
-    1i.*(cosd(beta).*sind(gamma).*cosd(theta)+(sind(beta).*sind(gamma).*cosd(phi-alpha)+...
-    cosd(gamma).*sind(phi-alpha)).*sind(theta))); % radian
-local_phi = local_phi.*180./pi; % degree
-
-end
