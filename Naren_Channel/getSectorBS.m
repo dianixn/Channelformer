f870aa3a064a064acb83ecf172e612b6b683b929
Dianxin Luan <49792786+dianixn@users.noreply.github.com>
2024-08-01 18:09:36 +0100

    Delete Naren_Channel/getSectorBS.m

--- Naren_Channel/getSectorBS.m
+++
@@ -1,8 +0,0 @@
-function sectorBS = getSectorBS(AOD_LOS)
-
-sectorBS = ones(size(AOD_LOS)).*2;
-
-sectorBS(logical(AOD_LOS>=-30)&&logical(AOD_LOS<90)) = 1;
-sectorBS(logical(AOD_LOS>=-150)&&logical(AOD_LOS<-30)) = 3;
-
-end
