0d3ff11597580922a1d098ca4d3e58fd9881fe2f
Dianxin Luan <49792786+dianixn@users.noreply.github.com>
2024-08-01 18:09:29 +0100

    Delete Naren_Channel/getPsiBS.m

--- Naren_Channel/getPsiBS.m
+++
@@ -1,9 +0,0 @@
-
-function psi = getPsiBS(theta,phi,alpha,beta,gamma) 
-% (M x N) x 1
-
-psi = angle((sind(gamma).*cosd(theta).*sind(phi-alpha)+cosd(gamma).*(cosd(beta).*sind(theta)-sind(beta).*cosd(theta).*cosd(phi-alpha)))...
-    +1i.*(sind(gamma).*cosd(phi-alpha)+sind(beta).*cosd(gamma).*sind(phi-alpha))); % radian
-psi = psi.*180./pi; % degree
-
-end
